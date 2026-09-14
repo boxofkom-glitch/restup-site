611ce6ebae7ebe11a230d72a344da841dd48060a
boxofkom-glitch <box.of.kom@gmail.com>
2026-09-14 23:53:37 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Lien de réservation, mailto: ou tel: professionnel à renseigner.
+// Valeur par défaut : email de contact déjà utilisé sur le reste du site.
+// À remplacer par un lien Cal.com si vous voulez une prise de rendez-vous synchronisée.
+window.RESTUP = { bookingUrl: "mailto:contact@restup-accompagnement.fr" };
